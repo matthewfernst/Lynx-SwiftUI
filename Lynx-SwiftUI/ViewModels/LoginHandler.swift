cdf3f755e747d84fb816ac4d1e0644d307959891
matthewfernst <matthew.f.ernst@gmail.com>
2023-11-25 20:58:20 -0800

    Google Sign in up and working

---
+++ Lynx-SwiftUI/ViewModels/LoginHandler.swift
@@ -0,0 +1,8 @@
+//
+//  LoginHandler.swift
+//  Lynx-SwiftUI
+//
+//  Created by Matthew Ernst on 11/25/23.
+//
+
+import Foundation
---
+++ Lynx-SwiftUI/Views/Login/GoogleSignIn.swift
@@ -0,0 +1,8 @@
+//
+//  GoogleSignIn.swift
+//  Lynx-SwiftUI
+//
+//  Created by Matthew Ernst on 11/25/23.
+//
+
+import Foundation
