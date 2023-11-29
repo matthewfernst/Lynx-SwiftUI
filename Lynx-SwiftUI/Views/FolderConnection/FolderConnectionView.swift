6d8ddb58f7d18e59a1a0b930fd0397d576c668c6
matthewfernst <matthew.f.ernst@gmail.com>
2023-11-29 07:15:20 -0800

    Year added to session summaries and upload view

---
+++ Lynx-SwiftUI/Views/FolderConnection/FolderConnectionView.swift
@@ -0,0 +1,18 @@
+//
+//  FolderConnectionView.swift
+//  Lynx-SwiftUI
+//
+//  Created by Matthew Ernst on 11/29/23.
+//
+
+import SwiftUI
+
+struct FolderConnectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FolderConnectionView()
+}
