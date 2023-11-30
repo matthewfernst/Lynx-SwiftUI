db338d297729e818af6f6b118f03b9a9681d42d8
matthewfernst <matthew.f.ernst@gmail.com>
2023-11-30 14:16:51 -0600

    Account view

---
+++ Lynx-SwiftUI/Views/Account/EditProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  EditProfileView.swift
+//  Lynx-SwiftUI
+//
+//  Created by Matthew Ernst on 11/29/23.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditProfileView()
+}
---
+++ Lynx-SwiftUI/Views/Account/GeneralSettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  GeneralSettingsView.swift
+//  Lynx-SwiftUI
+//
+//  Created by Matthew Ernst on 11/30/23.
+//
+
+import SwiftUI
+
+struct GeneralSettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GeneralSettingsView()
+}
