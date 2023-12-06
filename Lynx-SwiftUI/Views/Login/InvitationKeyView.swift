21cb3a00896737582a9a0db3730587752b3ba32c
matthewfernst <matthew.f.ernst@gmail.com>
2023-12-05 21:27:40 -0800

    Invitation key

---
+++ Lynx-SwiftUI/Views/Login/InvitationKeyView.swift
@@ -0,0 +1,18 @@
+//
+//  InvitationKeyView.swift
+//  Lynx-SwiftUI
+//
+//  Created by Matthew Ernst on 12/1/23.
+//
+
+import SwiftUI
+
+struct InvitationKeyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InvitationKeyView()
+}
