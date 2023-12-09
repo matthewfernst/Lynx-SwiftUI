8d06c3000457eae5d709988e11557b0328b91af1
matthewfernst <matthew.f.ernst@gmail.com>
2023-12-09 15:18:25 -0800

    Start of Leaderboards

---
+++ Lynx-SwiftUI/Models/TopLeaderAttributes.swift
@@ -0,0 +1,8 @@
+//
+//  TopLeaderAttributes.swift
+//  Lynx-SwiftUI
+//
+//  Created by Matthew Ernst on 12/9/23.
+//
+
+import Foundation
---
+++ Lynx-SwiftUI/Views/Leaderboard/CategoryTopLeadersView.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryTopLeadersView.swift
+//  Lynx-SwiftUI
+//
+//  Created by Matthew Ernst on 12/9/23.
+//
+
+import SwiftUI
+
+struct CategoryTopLeadersView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryTopLeadersView()
+}
